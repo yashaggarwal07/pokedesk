29c29008adaf35421be3ba09ac88c62f184e9ba4
yashaggarwal07 <ysinghal73@gmail.com>
2022-08-02 23:10:42 +0530

    popup

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,32 +4,47 @@ import PokemonDetailPopup from './PokemonDetailPopup'
 
 const Pokemon = (props) => {
     const { pokemon } = props;
-      return (
-        <div className="pokemon-card" >
-            <div className="pokemon-img-container">
-                <img
-                    src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-                    alt={pokemon.name}
-                    className="pokemon-img"
-                />
-            </div>
-            <div className="card-body">
-                <div className="card-top">
-                    <h3>{pokemon.name}</h3>
-                    <div>#{pokemon.id}</div>
+    const [pokemons, openPokemon] = useState([]);
+    const [cardClicked, setCardClick] = useState(false);
+
+    const onClick = async (e) => {
+        openPokemon(pokemon);
+        setCardClick(true)
+    };
+    return (
+        <div>
+            <div className="pokemon-card" onClick={onClick}>
+                <div className="pokemon-img-container">
+                    <img
+                        src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+                        alt={pokemon.name}
+                        className="pokemon-img"
+                    />
                 </div>
-                <div className="card-bottom">
-                    <div className="pokemon-type">
-                        {pokemon.types.map((type, idx) => {
-                            return (
-                                <div key={idx} className="pokemon-type-text">
-                                    {type.type.name}
-                                </div>
-                            );
-                        })}
+                <div className="card-body">
+                    <div className="card-top">
+                        <h3>{pokemon.name}</h3>
+                        <div>#{pokemon.id}</div>
+                    </div>
+                    <div className="card-bottom">
+                        <div className="pokemon-type">
+                            {pokemon.types.map((type, idx) => {
+                                return (
+                                    <div key={idx} className="pokemon-type-text">
+                                        {type.type.name}
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
+            {cardClicked ?
+                <><PokemonDetailPopup
+                    pokemon={pokemons} />
+                </>
+                : <div></div>
+            }
         </div>
     );
 };
--- src/components/PokemonDetailPopup.js
+++ src/components/PokemonDetailPopup.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const PokemonDetailPopup = (props) => {
+    const {pokemon} = props
  return (
-    <div>PokemonDetailPopup</div>
+    <div>
+          <div className="pokemon-img-container">
+                <img
+                    src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+                    alt={props.name}
+                    className="pokemon-img"
+                />
+            </div>
+            <div className="card-body">
+                <div className="card-top">
+                    <h3>{pokemon.name}</h3>
+                </div>
+                </div>
+    </div>
   )
 }
 
